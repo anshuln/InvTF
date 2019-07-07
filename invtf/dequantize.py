f81a39fa043abbf274ab79516a16aeee4f8d82c5
Alexander <alexander.mathiasen@gmail.com>
2019-07-07 15:43:13 +0200

    Filler code for dequantize code.

---
+++ invtf/dequantize.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import numpy as np
+
+class UniformDequantize(keras.layers.Layer):  pass 
+
+class VariationalDequantize(keras.layers.Layer):  pass  # maybe have model; learn this thing like in flow++ 
+
