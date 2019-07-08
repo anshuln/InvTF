22398fe22bee0c1120daa9b87f439beb8bc7d002
Alexander <alexander.mathiasen@gmail.com>
2019-07-08 18:39:33 +0200

    Placeholder for dequantize with some boilerplate code for uniform case.

--- invtf/dequantize.py
+++ invtf/dequantize.py
@@ -2,7 +2,20 @@ import tensorflow as tf
 import tensorflow.keras as keras
 import numpy as np
 
-class UniformDequantize(keras.layers.Layer):  pass 
+class UniformDequantize(keras.layers.Layer):  
+
+	def __init__(self, input_shape=None, amount=1.0): 
+		self.amount = amount
+		self.d		= input_shape
+		super(UniformDequantize, self).__init__(input_shape=input_shape)
+
+	def call(self, X): 
+		return X + tf.random.uniform(tf.shape(X), 0, 1)
+
+	def call_inv(self, Z): return Z
+		
+	def log_det(self): return 0.
+		
 
 class VariationalDequantize(keras.layers.Layer):  pass  # maybe have model; learn this thing like in flow++ 
 
