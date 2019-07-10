e8f88975ffc8539368ced721a6d561030778162a
Alexander <alexander.mathiasen@gmail.com>
2019-07-10 19:23:53 +0200

    Trains a simple multi-scale architecture. Not stable, proof of concept, need to refactor to work for other architectures.

--- example_mnist.py
+++ example_mnist.py
@@ -10,28 +10,30 @@ import datetime
 from tensorflow.keras.layers import ReLU, Dense
 from tensorflow.keras.models import Sequential
 
-from invtf.models import NICE
+from invtf.models import NICE, RealNVP
 
 import invtf.latent as latent
 
 
 X = mnist(digit=-1).images()
-g = NICE.mnist(X)
+X = X.reshape(60000, 28, 28, 1)
+#g = NICE.mnist(X)
+g = RealNVP.mnist(X)
 
 g.summary()
 
-#g.check_inv(X[:100], precision=10**(-0))  # I think uniform noise gets in here!! 
+#g.check_inv(X[:100], precision=10**(0))  # I think uniform noise gets in here!! 
 
 fig, ax = plt.subplots()
-
 
 log_dir			= "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard 	= keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-g.fit(X, batch_size=256, callbacks=[tensorboard], epochs=100)
+for _ in range(300): 
+	g.fit(X, batch_size=512, callbacks=[tensorboard], epochs=1)
 
-fake = g.sample(1, fix_latent=True)
+	fake = g.sample(1, fix_latent=True)
 
-ax.imshow(fake.reshape(28, 28), vmin=0, vmax=2**8) 
-plt.pause(.1)
+	ax.imshow(fake.reshape(28, 28), vmin=0, vmax=2**8) 
+	plt.pause(.1)
 
