361b52135dcc316bc30a4024ffab24556fb7ba18
Alexander <alexander.mathiasen@gmail.com>
2019-07-10 19:18:53 +0200

    Work on quantization.

--- invtf/dequantize.py
+++ invtf/dequantize.py
@@ -7,7 +7,7 @@ class UniformDequantize(keras.layers.Layer):
 	def __init__(self, input_shape=None, amount=1.0): 
 		self.amount = amount
 		self.d		= input_shape
-		super(UniformDequantize, self).__init__(input_shape=input_shape)
+		super(UniformDequantize, self).__init__(input_shape=input_shape)#input_shape)
 
 	def call(self, X): 
 		return X + tf.random.uniform(tf.shape(X), 0, 1)
