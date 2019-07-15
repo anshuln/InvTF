Adds const memory backprop for AffineCoupling and other layers. However, in some tests, the computed gradient is NaN for AffineCoupling, and this bug remains even if O(n) gradient calculations are used.

invtf/generator_const_backprop.py
@@ -8,7 +8,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_CPP_MIN_VLOG_LEVEL']='3'
 
-import tqdm
+from tqdm import tqdm
 import tensorflow as tf
 import invtf.grow_memory
 import tensorflow.keras as keras 
@@ -130,28 +130,28 @@ class Generator(keras.Sequential):
 
 
 
-	def predict(self, X, dequantize=True): 
+	# def predict(self, X, dequantize=True): 
 
-		Zs = [] 
+	# 	Zs = [] 
 
-		for layer in self.layers: 
+	# 	for layer in self.layers: 
 
-			# allow deactivating dequenatize 
-			# refactor to just look into name of layer and skip if it has dequantize in name or something like that. 
-			if not dequantize and isinstance(layer, UniformDequantize):     continue    
-			if not dequantize and isinstance(layer, VariationalDequantize): continue    
+	# 		# allow deactivating dequenatize 
+	# 		# refactor to just look into name of layer and skip if it has dequantize in name or something like that. 
+	# 		if not dequantize and isinstance(layer, UniformDequantize):     continue    
+	# 		if not dequantize and isinstance(layer, VariationalDequantize): continue    
 
-			# if isinstance(layer, MultiScale): 
-			# 	X, Z = layer.call(X)
-			# 	Zs.append(Z)
-			# 	continue
+	# 		# if isinstance(layer, MultiScale): 
+	# 		# 	X, Z = layer.call(X)
+	# 		# 	Zs.append(Z)
+	# 		# 	continue
 
-			X = layer.call(X)
+	# 		X = layer.call(X)
 
-		# TODO: make sure this does not break case without multiscale architecture.
-		# append Zs to X;; do by vectorize and then concat. 
+	# 	# TODO: make sure this does not break case without multiscale architecture.
+	# 	# append Zs to X;; do by vectorize and then concat. 
 
-		return X, Zs
+	# 	return X, Zs
 
 	def predict_inv(self, X, Z=None): 
 		n = X.shape[0]
@@ -220,17 +220,17 @@ class Generator(keras.Sequential):
 		last_layer = self.layers[-1]
 		#Computing gradients of loss function wrt the last acticvation
 		with tf.GradientTape() as tape:
-		    tape.watch(x)
-		    loss = self.loss(x)    #May have to change
+			tape.watch(x)
+			loss = self.loss(x)    #May have to change
 		grads_combined = tape.gradient(loss,[x])
 		dy = grads_combined[0]
 		y = x
 		#Computing gradients for each layer
 		for layer in self.layers[::-1]:     
-		    x = layer.call_inv(y)
-		    dy,grads = layer.compute_gradients(x,dy,layer.log_det)	#TODO implement scaling here...
-		    optimizer.apply_gradients(zip(grads,layer.trainable_variables))
-		    y = x 
+			x = layer.call_inv(y)
+			dy,grads = layer.compute_gradients(x,dy,layer.log_det)	#TODO implement scaling here...
+			optimizer.apply_gradients(zip(grads,layer.trainable_variables))
+			y = x 
 		return loss
 
 	def fit(self, X, batch_size=32,epochs=1,optimizer=tf.optimizers.Adam(),**kwargs): 
@@ -244,13 +244,14 @@ class Generator(keras.Sequential):
 			num_batches = X.shape[0] // batch_size
 			X = np.random.permutation(X)
 			#Minibatch gradient descent
-			for i in range(0,X.shape[0]-X.shape[0]%batch_size,batch_size):    
+			for i in tqdm(range(0,X.shape[0]-X.shape[0]%batch_size,batch_size)):    
+				print("Minibatch: ",i)
 			# grads,loss = model.compute_gradients(X[i:(i+batch_size)])
 				losses = []
 				loss = self.compute_and_apply_gradients(X[i:(i+batch_size)],optimizer)
 				losses.append(loss.numpy())
 				loss = np.mean(losses)  
-			print(loss)
+			print('Epoch: {}, loss: {}'.format(j,loss))
 			all_losses+=losses
 		return all_losses
 

invtf/layers_const_backprop.py
@@ -364,7 +364,7 @@ class Squeeze(LayerWithGrads):
 	def call_inv(self, X): 
 		return tf.reshape(X, [-1, self.w, self.h, self.c])
 		
-	def log_det(self): return 0. 
+	def log_det(self): return tf.zeros((1,)) 
 
 
 # TODO: for now assumes target is +-1, refactor to support any target. 
@@ -405,7 +405,7 @@ class MultiScale(keras.layers.Layer):
 		n, h, w, c = input_shape
 		return (n, h, w, c//2)
 
-	def log_det(self): return 0.
+	def log_det(self): return tf.zeros((1,))
 
 
 
@@ -488,7 +488,7 @@ class CircularConv(keras.layers.Layer):
 	def log_det(self):      pass
 
 
-class ActNorm(keras.layers.Layer): 
+class ActNorm(LayerWithGrads): 
 
 	"""
 		The exp parameter allows the scaling to be exp(s) \odot X. 
@@ -610,8 +610,10 @@ class AffineCoupling(LayerWithGrads): # Sequential):
 			x1 		= x1*s + t 
 
 		self.precompute_log_det(s, X)
-
+		# print("s",np.isnan(s),np.isnan(t))
 		X 		= self.strategy.combine(x0, x1)
+		print("s",np.isnan(s).all(),"t",np.isnan(t).all())
+		print("X0",np.isnan(x0).all(),"X1",np.isnan(x1).all())
 		return X
 
 	def call_inv(self, Z):	 
@@ -637,3 +639,33 @@ class AffineCoupling(LayerWithGrads): # Sequential):
 
 	def summary(self, line_length=None, positions=None, print_fn=None):
 		print_summary(self, line_length=line_length, positions=positions, print_fn=print_fn) # fixes stupid issue.
+
+	def compute_gradients(self,x,dy,regularizer=None):  
+		'''
+		Computes gradients for backward pass
+		Args:
+			x - tensor compatible with forward pass, input to the layer
+			dy - incoming gradient from backprop
+			regularizer - function, indicates dependence of loss on weights of layer
+		Returns
+			dy - gradients wrt input, to be backpropagated
+			grads - gradients wrt weights
+		'''
+		#TODO check if log_det of AffineCouplingLayer depends needs a regularizer. -- It does
+		with tf.GradientTape(persistent=False) as tape:	#Since log_det is computed within call
+			tape.watch(x)
+			y_ = self.call(x)   #Required to register the operation onto the gradient tape
+			reg = self._log_det
+		#TODO known issue, gradient goes to nan in some cases...
+		grads_combined = tape.gradient(y_,[x]+self.trainable_variables,output_gradients=dy)
+		grads_wrt_reg = tape.gradient(reg,self.trainable_variables)
+		dy,grads = grads_combined[0],grads_combined[1:]
+		grads = [a[0]+a[1] for a in zip(grads,grads_wrt_reg)]
+		del tape 	#Since tape was persistent, we need this
+
+
+		# if regularizer is not None:
+		# 	with tf.GradientTape() as tape:
+		# 		reg = -regularizer()
+		# 	grads_wrt_reg = tape.gradient(reg, self.trainable_variables)
+		return dy,grads