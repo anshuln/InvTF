Fixes order of gradients in tests

test/gradients.py
@@ -12,6 +12,8 @@ from invtf.layers_const_backprop import *
 from tensorflow.keras.layers import ReLU, Dense, Flatten, Conv2D	
 
 class GeneratorGradTest(GenConst):
+	def prune(self,l):
+		return [x for sublist in l for x in sublist if len(sublist)>0]
 	def compute_gradients(self,X):
 		x = self.call(X)        #I think putting this in context records all operations onto the tape, thereby destroying purpose of checkpointing...
 		last_layer = self.layers[-1]
@@ -28,12 +30,9 @@ class GeneratorGradTest(GenConst):
 		for layer in self.layers[::-1]:     
 			x = layer.call_inv(y)
 			dy,grads = layer.compute_gradients(x,dy,layer.log_det,d*np.log(2.))	#TODO implement scaling here -- DONE
-			gradients+=(grads)
+			gradients=[grads]+gradients
 			y = x 
-		if len(gradients) > 1:	#TODO better fix for issue with only single gradient
-			return [gradients[-1]]+[gradients[-2]]+gradients[0:-2][::-1]
-		else:
-			return gradients
+		return self.prune(gradients)
 
 	def actual_gradients(self,X):
 		with tf.GradientTape() as tape:
@@ -47,34 +46,34 @@ class TestGradients(unittest.TestCase):
 	def assertGrad(self,g,X):
 		computed_grads = g.compute_gradients(X)
 		actual_grads = g.actual_gradients(X) 
-		A = [np.allclose(np.abs(x[0]-x[1]),0,atol=2, rtol=0.1) for x in zip(computed_grads,actual_grads) if x[0] is not None]
-		print("computed",computed_grads,"actual_grads",actual_grads)
+		A = [np.allclose(np.abs(x[0]-x[1]),0,atol=1, rtol=0.1) for x in zip(computed_grads,actual_grads) if x[0] is not None]
+		# print("computed",computed_grads,"actual_grads",actual_grads)
 		print("Max discrepancy in gradients",np.max(np.array([np.max((np.abs(x[0]-x[1]))) for x in zip(computed_grads,actual_grads) if x[0] is not None])))
 		self.assertTrue(np.array(A).all())
 
-	# def test_circ_conv(self):
-	# 	X = TestGradients.X 
-	# 	d = 32*32*3
-	# 	g = GeneratorGradTest(invtf.latent.Normal(d)) 
-	# 	g.add(Conv3DCirc())
-	# 	g.predict(X[:1])
-	# 	self.assertGrad(g,X)		
+	def test_circ_conv(self):
+		X = TestGradients.X 
+		d = 32*32*3
+		g = GeneratorGradTest(invtf.latent.Normal(d)) 
+		g.add(Conv3DCirc())
+		g.predict(X[:1])
+		self.assertGrad(g,X)		
 
-	# def test_inv_conv(self):
-	# 	X = TestGradients.X 
-	# 	d = 32*32*3
-	# 	g = GeneratorGradTest(invtf.latent.Normal(d)) 
-	# 	g.add(Inv1x1ConvPLU())
-	# 	g.predict(X[:1])
-	# 	self.assertGrad(g,X)		
+	def test_inv_conv(self):
+		X = TestGradients.X 
+		d = 32*32*3
+		g = GeneratorGradTest(invtf.latent.Normal(d)) 
+		g.add(Inv1x1ConvPLU())
+		g.predict(X[:1])
+		self.assertGrad(g,X)		
 
-	# def test_act_norm(self):
-	# 	X = TestGradients.X 
-	# 	d = 32*32*3
-	# 	g = GeneratorGradTest(invtf.latent.Normal(d)) 
-	# 	g.add(ActNorm())
-	# 	g.predict(X[:1])
-	# 	self.assertGrad(g,X)		
+	def test_act_norm(self):
+		X = TestGradients.X 
+		d = 32*32*3
+		g = GeneratorGradTest(invtf.latent.Normal(d)) 
+		g.add(ActNorm())
+		g.predict(X[:1])
+		self.assertGrad(g,X)		
 
 	def test_affine_coupling(self):
 		X = np.random.normal(0,1,(5,2,2,2)).astype('f')